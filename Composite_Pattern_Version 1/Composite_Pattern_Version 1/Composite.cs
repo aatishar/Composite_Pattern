7ad42e1c2b151261edd3a41b440f58769aceab74
aatish.ar <aatish.ar123@gmail.com>
2021-08-20 14:46:38 +0400

    Composite Pattern Version 1: init

---
+++ Composite_Pattern_Version 1/Composite_Pattern_Version 1/Composite.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_Pattern_Version_1
+{
+    class Composite
+    {
+        public Composite Parent;
+        public List<Composite> Children = new List<Composite>();
+        private decimal price;
+
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                this.PropagatePriceChangeToParent();
+            }
+        }
+        public void AddChild(Composite composite)
+        {
+            this.Children.Add(composite);
+            composite.Parent = this;
+            this.Price = this.TotalPrice();
+        }
+
+        public void RemoveChild(Composite composite)
+        {
+            this.Children.Remove(composite);
+            this.Price = this.TotalPrice();
+        }
+
+        public void PropagatePriceChangeToParent()
+        {
+            this.Parent?.ReCalculateTotal();
+        }
+
+        public void ReCalculateTotal()
+        {
+            this.Price = this.Children.Sum(a => a.Price);
+        }
+
+        public decimal TotalPrice()
+        {
+            if (this.Children.Count == 0)
+            {
+                return this.Price;
+            }
+            else
+            {
+                return this.Children
+                   .Select(a => a.TotalPrice())
+                   .Sum();
+            }
+
+        }
+    }
+}
---
+++ Composite_Pattern_Version 1/Composite_Pattern_Version 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Composite_Pattern_Version_1
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Composite();
+            var p1 = new Composite();
+            var p2 = new Composite();
+            var p3 = new Composite();
+            var c1 = new Composite { Price = 14 };
+            var c2 = new Composite { Price = 15 };
+            var c3 = new Composite { Price = 16 };
+
+            root.AddChild(p1);
+            root.AddChild(p2);
+
+            p1.AddChild(c1);
+            p1.AddChild(c2);
+
+            p2.AddChild(p3);
+
+            p3.AddChild(c3);
+
+            Console.WriteLine(root.TotalPrice());
+            Console.WriteLine(root.Price);
+            Console.WriteLine(p1.Price);
+            Console.WriteLine(p2.Price);
+            Console.WriteLine(p3.Price);
+        }
+    }
+}
